38dda7ec68464a701ae6b1c4299e6372679e8a6c
sallenray <allenray20@gmail.com>
2016-11-16 13:41:40 +0800

    20161116_v1

---
+++ BasicCalc/BasicCalc/BasicCalc.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace WindowsFormsApplication11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNumA = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNumB = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtLogic = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNumA
+            // 
+            this.txtNumA.Location = new System.Drawing.Point(52, 22);
+            this.txtNumA.Name = "txtNumA";
+            this.txtNumA.Size = new System.Drawing.Size(100, 22);
+            this.txtNumA.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "數字A";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "數字B";
+            // 
+            // txtNumB
+            // 
+            this.txtNumB.Location = new System.Drawing.Point(52, 95);
+            this.txtNumB.Name = "txtNumB";
+            this.txtNumB.Size = new System.Drawing.Size(100, 22);
+            this.txtNumB.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "運算子";
+            // 
+            // txtLogic
+            // 
+            this.txtLogic.Location = new System.Drawing.Point(52, 64);
+            this.txtLogic.Name = "txtLogic";
+            this.txtLogic.Size = new System.Drawing.Size(100, 22);
+            this.txtLogic.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(159, 73);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(98, 12);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "1:加,2:減,3:乘,4:除";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(76, 150);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "計算";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtLogic);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNumB);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNumA);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNumA;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNumB;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtLogic;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ BasicCalc/BasicCalc/BasicCalc.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication11
+{
+    public partial class Form1 : Form
+    {
+        int numberA, numberB;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(txtNumA.Text.Trim(), out numberA) && int.TryParse(txtNumB.Text.Trim(), out numberB))
+            {
+                switch (txtLogic.Text.Trim())
+                {
+                    case "":
+                    case "1":
+                        Show(numberA, numberB, "+");
+                        break;
+                    case "2":
+                        Show(numberA, numberB, "-");
+                        break;
+                    case "3":
+                        Show(numberA, numberB, "*");
+                        break;
+                    case "4":
+                        Show(numberA, numberB, "/");
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else
+            {
+                MessageBox.Show("請輸入要計算的數字!!");
+            }
+        }
+
+        private void Show(int intA, int intB, string strLogic)
+        {
+            if (!(intB == 0 && strLogic == "/"))
+            {
+                MessageBox.Show((intA + strLogic + intB).ToString());
+            }
+            else
+            {
+                MessageBox.Show("無法除於0，數字B請輸入非0值 !!");
+            }
+        }
+    }
+}
